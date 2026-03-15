1d876ac0114fe385ad14bce2ebaf41fb79428fe3
ssjesusalvarez <ssjesusalvarez@gmail.com>
2026-03-15 03:19:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+datos = []
+
 @app.route("/")
 def home():
     return "Server running"
@@ -9,8 +11,12 @@ def home():
 @app.route("/data", methods=["POST"])
 def data():
     json_data = request.json
-    print(json_data)
-    return jsonify({"status": "ok", "received": json_data})
+    datos.append(json_data)
+    return jsonify({"status":"ok","received":json_data})
+
+@app.route("/getdata", methods=["GET"])
+def getdata():
+    return jsonify(datos)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
